45ae00f14770575fb2233b82f20f05df373b8cb5
kapil <kapil.goud14@gmail.com>
2020-07-06 16:06:48 +0530

    Final Commit.

--- PromotionalEngineTest/CheckoutProcessTest.cs
+++ PromotionalEngineTest/CheckoutProcessTest.cs
@@ -297,6 +297,33 @@ namespace Tests
 
         }
 
+        [Test]
+        public void TestForProductABCDfinal2Item()
+        {
+            var cart = new Cart
+            {
+                Products = new List<Product> {
+                new Product { Id = 1, Name = "A", Price = 50 },
+                new Product { Id = 1, Name = "A", Price = 50 },
+                new Product { Id = 1, Name = "A", Price = 50 },
+                new Product { Id = 1, Name = "A", Price = 50 },
+                new Product { Id = 2, Name = "B", Price = 30 },
+                new Product { Id = 2, Name = "B", Price = 30 },
+                new Product { Id = 2, Name = "B", Price = 30 },
+                new Product { Id = 2, Name = "B", Price = 30 },
+                new Product { Id = 2, Name = "B", Price = 30 },
+                new Product {Id=4,Name="D",Price=15},
+                new Product {Id=3,Name="C",Price=20},
+
+            }
+            };
+
+            var checkout = new Checkout();
+            var result = checkout.CalculateCartPrice(cart);
+
+            Assert.AreEqual(330, result);
+
+        }
 
     }
 }
